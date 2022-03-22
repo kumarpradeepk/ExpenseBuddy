c5a693f8b32b309d01f722431800d0fd7a5a4b65
pradeep.k <Qwerty123!>
2022-03-22 22:02:56 +0530

    Adding the ViewModel

---
+++ app/src/main/java/kumar/pradeep/expensebuddy/feature/domain/model/Transaction.kt
@@ -0,0 +1,12 @@
+package kumar.pradeep.expensebuddy.feature.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Transaction(
+ @PrimaryKey val id:Int? = null,
+ val title: String,
+ val category: String,
+ val timestamp: Long
+)
